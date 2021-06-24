264c92543091d4c7207fad9b8acf64bd0e92ada6
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-06-24 13:21:07 +0200

    move camera rig

--- teleport.js
+++ teleport.js
@@ -74,10 +74,10 @@ AFRAME.registerComponent('blink-teleportation', {
                     //  var newPos = objectToPos(incomingData.position);
 
                     camRig.removeAttribute('wasd-controls');
-                    //  data.cameraRig.setAttribute('position', data.pos);
+                    data.cameraRig.setAttribute('position', data.pos);
 
-                    camRig.setAttribute('position', data.pos)
-                        //   cart.setAttribute('position', " "+data.pos.x+"0.10"+data.pos.z);
+                    // camRig.setAttribute('position', data.pos)
+                    //   cart.setAttribute('position', " "+data.pos.x+"0.10"+data.pos.z);
                     cart.object3D.position.x = data.pos.x;
                     cart.object3D.position.z = (data.pos.z - 1);
 
