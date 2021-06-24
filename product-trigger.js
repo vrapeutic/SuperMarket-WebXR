1cf0de2d1bb1c7d771dee8a5f4861e585ca51ca3
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-06-24 17:32:57 +0200

    revert again

--- product-trigger.js
+++ product-trigger.js
@@ -1,10 +1,9 @@
 window.correctProducts = 0;
 window.wrongProducts = 0;
-const ADD_VALUE = 0.1;
 
-window.x=0.1;
+window.x=0;
  window.y=0;
-window.z=0.1;
+window.z=0;
 AFRAME.registerComponent('product-collector', {
   schema: {
     myType: {type: 'string', default: 'correct'}
@@ -17,6 +16,7 @@ AFRAME.registerComponent('product-collector', {
    var correctProduct;
    var wrongProduct;
    console.log('corrrect '+el);
+
   el.addEventListener("correctCollect", function(){
    // hlioynntrgnfl correctProduct=document.querySelectorAll('.newItems');  
      console.log('corrrect product'+el);
@@ -26,24 +26,15 @@ el.setAttribute('static-body','enabled:true');
 setTimeout(() => {
   var new_element = el.cloneNode(true);
     el.parentNode.replaceChild(new_element,el);
+   // el.setAttribute("position",el.getAttribute('position'));
     console.log(window.z+""+el.object3D.position.z);
-//new_element.object3D.position.z=0;
-//new_element.object3D.position.y=0;
+
 //window.x=(el.object3D.position.x- document.getElementById('trolly').getAttribute("position").x)-1;
 //window.z=el.object3D.position.z+ document.getElementById('trolly').getAttribute("position").z;
  //window.y=el.object3D.position.y- document.getElementById('trolly').getAttribute("position").y;
  console.log(window.z+""+el.object3D.position.z);
-if(window.x>=0.2)
- window.x=window.x-0.1;
- else if(window.x<0.2&&window.z<=0.7)
- window.z=window.z+0.2;
-//else window.z=window.z+0.1;
-else if (window.z>=0.7)
-window.x=window.x+0.1;
-
-  new_element.setAttribute("position",""+window.x+" 0.6 "+window.z);
-
- document.getElementById('trolly').appendChild(new_element); 
+ document.getElementById('trolly').firstElementChild.appendChild(new_element); 
+ //  new_element.setAttribute("position",{x:window.x,y:window.y,z:window.z});
 
     }, 2000);
   })
@@ -60,7 +51,7 @@ window.x=window.x+0.1;
   var new_element =el.cloneNode(true);
  el.parentNode.replaceChild(new_element,el);
        document.getElementById('trolly').firstElementChild.appendChild(new_element); 
-  new_element.setAttribute("position",{x:window.x,y:0,z:0.2});
+ //  new_element.setAttribute("position",{x:window.x,y:0,z:0.2});
 
   }, 2000);
 
