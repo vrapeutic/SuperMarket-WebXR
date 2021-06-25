0149296dd8412b873c8207863b6c40270dcbfd69
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-06-25 15:59:09 +0200

    Update teleport.js

--- teleport.js
+++ teleport.js
@@ -6,8 +6,12 @@ AFRAME.registerComponent('teleportation', {
         var el = this.el
         let pos = el.object3D.position
         let camRig = document.querySelector('#camRig');
-        let cam = document.querySelector('#cam');
-        cam.setAttribute('camera', 'active', true);
+        let cam = document.querySelectorAll('[camera]');
+        cam.forEach(camera => {
+            if (camera.parentElement.id != 'camRig')
+                camera.setAttribute('camera', 'active', false);
+
+        });
         el.addEventListener('click', function() {
             camRig.setAttribute('animation', 'property: position;to:' + pos.x + '' + pos.y + '' + pos.z + ';dur: 1000')
         });
