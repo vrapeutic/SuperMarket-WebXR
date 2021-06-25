8ca4ee99ee588ea95ba5d6dd160f590013a7f864
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-06-25 16:13:32 +0200

    Update teleport.js

--- teleport.js
+++ teleport.js
@@ -6,10 +6,20 @@ AFRAME.registerComponent('teleportation', {
         var el = this.el
         let pos = el.object3D.position
         let camRig = document.querySelector('#camRig');
-        let cam = document.querySelectorAll('[camera]');
-        cam.forEach(camera => {
-            if (camera.parentElement.id != 'camRig')
-                camera.setAttribute('camera', 'active', false);
+
+
+        /**/
+        document.querySelector('a-scene').addEventListener('enter-vr', function() {
+            let cam = document.querySelectorAll('[camera]');
+            //  cam[0].parentNode.removeChild(cam[0]);
+            cam.forEach(camera => {
+                if (camera.parentElement.id != 'camRig') {
+                    camera.setAttribute('camera', 'active', false);
+                    camera.parentNode.removeChild(camera);
+                }
+                console.log(camera);
+
+            });
 
         });
         el.addEventListener('click', function() {
