c81404d29739ef63a7c0ed76f5b952aec49b38ad
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-06-25 17:37:21 +0200

    Update teleport.js

--- teleport.js
+++ teleport.js
@@ -30,34 +30,34 @@ AFRAME.registerComponent('blink-teleportation', {
 
                 }
                 // DETECTING DEVICE AND SETTING SELECTIVE INTERSECTIONS
-                if (!isMobileVR && !isHeadsetConnected) {
-                    // Standard Desktop
-                    var cursor = document.createElement('a-cursor');
-                    // cursor.setAttribute('raycaster', 'objects', data.raycasterObjects);
-                    data.camRig.appendChild(cursor);
-                } else if (isMobileVR || isHeadsetConnected) {
-                    if (data.dof === 3) {
-                        // Oculus Go
-                        var controller_1 = document.createElement('a-entity');
-                        // controller_1.setAttribute('laser-controls', 'hand', data.hand);
-                        //controller_1.setAttribute('raycaster', 'objects', data.raycasterObjects);
-                        data.camRig.appendChild(controller_1);
-                    } else if (data.dof === 6) {
-                        // Oculus Quest || Rift S, Rift, and (not tested but it should work) HTC Vive
-                        var controller_RH = document.createElement('a-entity');
-                        ////controller_RH.setAttribute('laser-controls', 'hand', 'right');
-                        // controller_RH.setAttribute('raycaster', 'objects', data.raycasterObjects);
-                        // controller_RH.setAttribute('raycaster', 'far', '100');
-                        controller_RH.setAttribute('garb', 'enabled', true);
-                        var controller_LH = document.createElement('a-entity');
-                        // controller_LH.setAttribute('laser-controls', 'hand', 'left');
-                        //  controller_LH.setAttribute('raycaster', 'objects', data.raycasterObjecs);
-                        controller_LH.setAttribute('garb', 'enabled', true);
+                /*  if (!isMobileVR && !isHeadsetConnected) {
+                      // Standard Desktop
+                      var cursor = document.createElement('a-cursor');
+                      // cursor.setAttribute('raycaster', 'objects', data.raycasterObjects);
+                      data.camRig.appendChild(cursor);
+                  } else if (isMobileVR || isHeadsetConnected) {
+                      if (data.dof === 3) {
+                          // Oculus Go
+                          var controller_1 = document.createElement('a-entity');
+                          // controller_1.setAttribute('laser-controls', 'hand', data.hand);
+                          //controller_1.setAttribute('raycaster', 'objects', data.raycasterObjects);
+                          data.camRig.appendChild(controller_1);
+                      } else if (data.dof === 6) {
+                          // Oculus Quest || Rift S, Rift, and (not tested but it should work) HTC Vive
+                          var controller_RH = document.createElement('a-entity');
+                          ////controller_RH.setAttribute('laser-controls', 'hand', 'right');
+                          // controller_RH.setAttribute('raycaster', 'objects', data.raycasterObjects);
+                          // controller_RH.setAttribute('raycaster', 'far', '100');
+                          controller_RH.setAttribute('garb', 'enabled', true);
+                          var controller_LH = document.createElement('a-entity');
+                          // controller_LH.setAttribute('laser-controls', 'hand', 'left');
+                          //  controller_LH.setAttribute('raycaster', 'objects', data.raycasterObjecs);
+                          controller_LH.setAttribute('garb', 'enabled', true);
 
-                        data.camRig.appendChild(controller_RH);
-                        data.camRig.appendChild(controller_LH);
-                    }
-                }
+                          data.camRig.appendChild(controller_RH);
+                          data.camRig.appendChild(controller_LH);
+                      }
+                  }*/
 
                 // CREATE A TRANSPARENT BLACK IMAGE
                 var blink = document.createElement('a-image');
